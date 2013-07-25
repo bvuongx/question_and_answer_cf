54107d3071179504f4604f469010f823baf1199b
Meredith Underell <m.underell@gmail.com>
2013-07-25 13:21:44 -0700

    Add title functionality for show page

--- spec/integration/creating_questions_spec.rb
+++ spec/integration/creating_questions_spec.rb
@@ -7,5 +7,10 @@ feature 'Creating Questions' do
     fill_in 'Inquiry', :with => 'What is Code Fellows?'
     click_button 'Create Question'
     page.should have_content('Question has been posted.')
+
+    question = Question.find_by_inquiry("What is Code Fellows?")
+    page.current_url.should == question_url(question)
+    title = "What is Code Fellows? - Question - QandA"
+    find("title").should have_content(title)
   end
 end
