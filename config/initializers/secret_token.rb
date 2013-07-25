5d3deaafc56dce7f1ce96435dc8c1ce85e82e517
Meredith Underell <m.underell@gmail.com>
2013-07-24 18:28:24 -0700

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+QAndA::Application.config.secret_token = 'eab4e93d88c01748f792b07ead48e2e7bf0db7f02a2fe3c6269199120f168ad5d9d2e5c2cf6d29157143d73b151afd06a609134f226d5a3d62d453c8c90cea1f'
